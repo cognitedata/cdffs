047abddfc191b14a67af962ca39db5bd726ad427
Infant Alex <infant.alex@cognite.com>
2024-02-01 12:29:55 +0100

    Updated dependency versions (#123)
    * Updated dependency versions
    * Poetry updates
    * Github actions updateS
    * version updates

--- cognite/cdffs/__init__.py
+++ cognite/cdffs/__init__.py
@@ -3,7 +3,7 @@ import fsspec
 
 from .spec import CdfFileSystem
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 __all__ = ["CdfFileSystem"]
 
 fsspec.register_implementation(CdfFileSystem.protocol, CdfFileSystem)
